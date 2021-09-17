63beeb549e6555b2259258a58ca558ceed29403e
dpisati <43629737+dpisati@users.noreply.github.com>
2021-09-18 10:11:16 +1200

    first commit

---
+++ src/components/NavBar/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { NavBarContainer } from './styles'
+export default function NavBar() {
+    return (
+        <NavBarContainer>
+            header
+        </NavBarContainer>
+    )
+}
---
+++ src/components/NavBar/styles.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const NavBarContainer = styled.div`
+    background-color: red;
+    width: 100%;
+    height: 66px;
+`;
